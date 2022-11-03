275e85c7ab7ffb8a4f23c01a7cff1860529709f6
Bishar00 <bishar1yusuf@gmail.com>
2022-11-03 11:52:35 -0500

    example

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -5,6 +5,7 @@
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
         let lastName = 'Yusuf'
         console. log (Yusuf);
+        
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 
